24be7dfe1e7650819f8f23e60b8e3c8e31a65a0e
今天吃没吃123 <13580127+have-you-eaten-today-123@user.noreply.gitee.com>
2024-08-25 00:03:49 +0800

    面试模块新增interviewArrange部分

---
+++ src/components/interviewArrangement.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+import {PropTypes} from '@/utils/propTypes'
+import titleBlock from '@/components/titleBlock.vue'
+defineOptions({
+  name:'interviewArrangement'
+})
+
+const prop=defineProps({
+  startTime:PropTypes.string.def('2022-12-12'),
+  endTime:PropTypes.string.def('1314--05-20'),
+  address:PropTypes.string.def('松山湖校区:9A206-3'),
+  description:PropTypes.string.def('进入面试请检查相关设备是否正常的撒合法空间的开了房间啊打开')
+})
+</script>
+
+<template>
+  <titleBlock title="面试安排" class="Itarrangement-title"></titleBlock>
+  <div class="arrangement-card">
+        <n-flex vertical>
+          <div class="arrangement-content">
+            <p class="arrangement-title">面试时间:</p>
+            <p class="arrangement-description"><span>{{ prop.startTime }} — </span><span>{{ prop.endTime }}</span></p>
+          </div>
+          <div class="arrangement-content">
+            <p class="arrangement-title">面试地点:</p>
+            <p class="arrangement-description">{{ prop.address }}</p>
+          </div>
+          <div class="arrangement-content">
+            <p class="arrangement-title">面试说明</p>
+            <p class="last-description">{{ prop.description }}</p>
+          </div>
+        </n-flex>
+    </div>
+</template>
+
+<style scoped>
+.Itarrangement-title{
+  margin: 2vh 0 0 4vw;
+  font-size: 1.5rem;
+}
+.arrangement-card{
+  width: 90vw;
+  min-height: 20vh;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgb(137, 148, 173) ;
+  margin: 0 auto 0 5vw;
+}
+.arrangement-content{
+  font-family: '楷体';
+  font-size: 1.1rem;
+}
+.arrangement-title{
+  padding: 2vh 0 0 3vw;
+  font-size: 1.1rem;
+}
+.arrangement-description{
+  width: 75vw;
+  padding: 1vh 0 0 8vw;
+}
+.last-description{
+  width: 75vw;
+  padding: 1vh 0 2vh 8vw;
+}
+</style>
--- src/views/interview.vue
+++ src/views/interview.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
 import interviewResult from '@/components/interviewResult.vue'
+import interviewArrangement from '@/components/interviewArrangement.vue'
+import { ref } from 'vue'
+const interviewTurn=ref('初试')
 </script>
 
 <template>
   <div class="interview-layout">
+    <p class="interview-name">{{ interviewTurn }}</p>
     <interviewResult></interviewResult>
+    <interviewArrangement></interviewArrangement>
   </div>
 </template>
 
@@ -12,5 +17,10 @@ import interviewResult from '@/components/interviewResult.vue'
 .interview-layout{
   width: 100vw;
   min-height: 80vh;
+}
+.interview-name{
+  font-size: 1.6rem;
+  font-weight: bolder;
+  margin: 3vh 0 0 40vw;
 }
 </style>
